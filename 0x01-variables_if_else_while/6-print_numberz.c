fcb836d579f89c3340162161d88d736443a6046e
Ayobami Oludemi <oludemiayobami@gmail.com>
2023-06-16 22:46:33 +0100

    0x01 Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+* main - Prints out numbers 0-9 from their ascii codes
+*
+* Return: int 0
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
