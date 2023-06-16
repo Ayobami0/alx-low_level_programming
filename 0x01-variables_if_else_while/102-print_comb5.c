12f3cb44f1dc77c7830e0724f3d63c3c7ac57d01
Ayobami Oludemi <oludemiayobami@gmail.com>
2023-06-17 00:56:36 +0100

    0x01 Task 102(Advanced)

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#define STOP 58 /* : in ascii. 9 is 58 - 1 */
+#define START 48 /* 0 in ascii */
+
+/**
+* main - prints out 3 digit combinations of digits from 0-9
+* Each digits are unique and there is no repetiton of combination
+* All combinations are in accending order.
+*
+* Return: 0
+*/
+int main(void)
+{
+	int i, j, k, l;
+
+	for (i = START; i < STOP; i++)
+	{
+		for (j = START; j < STOP; j++)
+		{
+			for (k = START; k < STOP; k++)
+			{
+				for (l = j+1; l < STOP; l++)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(' ');
+					putchar(k);
+					putchar(l);
+					/* if (i != (STOP - 1) )
+					{
+						putchar(',');
+						putchar(' ');
+					}*/
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
