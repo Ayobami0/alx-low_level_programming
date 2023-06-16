e5b24195198e33b79924604480b36e0ed099bf22
Ayobami Oludemi <oludemiayobami@gmail.com>
2023-06-16 02:28:32 +0100

    Task 6 Fix

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,17 +1,23 @@
 #include <stdio.h>
 
-int main(void) {
-  int charSize = sizeof(char);
-  int intSize = sizeof(int);
-  int longIntSize = sizeof(long int);
-  int longLongIntSize = sizeof(long long int);
-  int floatSize = sizeof(float);
+/**
+ * main - Main entry to the program
+ * 
+ * Return: int
+*/
+int main(void) 
+{
+	int charSize = sizeof(char);
+	int intSize = sizeof(int);
+	int longIntSize = sizeof(long int);
+	int longLongIntSize = sizeof(long long int);
+	int floatSize = sizeof(float);
 
-  printf("Size of a char: %i byte(s)\n", charSize);
-  printf("Size of an int: %i byte(s)\n", intSize);
-  printf("Size of a long int: %i byte(s)\n", longIntSize);
-  printf("Size of a long long int: %i byte(s)\n", longLongIntSize);
-  printf("Size of a float: %i byte(s)\n", floatSize);
+	printf("Size of a char: %i byte(s)\n", charSize);
+	printf("Size of an int: %i byte(s)\n", intSize);
+	printf("Size of a long int: %i byte(s)\n", longIntSize);
+	printf("Size of a long long int: %i byte(s)\n", longLongIntSize);
+	printf("Size of a float: %i byte(s)\n", floatSize);
 
   return (0);
 }
