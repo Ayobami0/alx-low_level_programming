5c003689f75a2622b75f5b5d497e3a69c42ccc3b
Ayobami Oludemi <oludemiayobami@gmail.com>
2023-06-20 23:39:57 +0100

    0x02

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* add - Adds 2 integers
+* @a: first integer
+* @b: second integer
+*
+* Return: Result of the addition
+*/
+int add(int a, int b)
+{
+	int result = a + b;
+	return (result);
+}
+
---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* print_to_98 - Prints all natural numbers
+* from number n to 98
+* @n: start number
+*/
+void print_to_98(int n)
+{
+	if (n > 98)
+	{
+		while (n > 98)
+		{
+			printf("%d, ", n--);
+		}
+	}
+	else 
+	{
+		while (n < 98)
+		{
+			printf("%d, ", n++);
+		}
+	}
+}
+
