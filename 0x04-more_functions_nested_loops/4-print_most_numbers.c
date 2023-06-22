3151f926a26daf54fa6450e882c037d20f771b26
Ayobami Oludemi <oludemiayobami@gmail.com>
2023-06-22 22:43:21 +0100

    0x04 Task 4: create file

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* print_numbers - print numbers from 0 - 9
+* with the exception of 2 and 4 ending with a new line.
+*/
+void print_numbers(void)
+{
+	int n;
+
+	n = 48;
+	while (n < 58)
+	{
+		if (n != ('0' + 2) && n != '0' + 2)
+			_putchar(n);
+		n++;
+	}
+	_putchar('\n');
+}
+
+
