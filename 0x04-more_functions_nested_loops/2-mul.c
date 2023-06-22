f2e58c3d8d498c975680aac46beb8dadf5c7ef87
Ayobami Oludemi <oludemiayobami@gmail.com>
2023-06-22 22:18:24 +0100

    0x04 Task 1: fix main.h

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* mul - multiplies two integers and
+* returns the value.
+* @a: number 1
+* @b: number 2
+*
+* Return: value of multiplation of
+* @a and @b
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
