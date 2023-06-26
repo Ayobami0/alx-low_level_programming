aa6f58194e6b9e401e4888cac20398eda7ee8d3a
Ayobami Oludemi <oludemiayobami@gmail.com>
2023-06-26 22:46:24 +0100

    0x05 Task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+/**
+ * _strlen - counts the number of characters in
+ * a given string @s
+ * @s: string to count
+ *
+ * Return: count
+ * number of character i a string
+*/
 int _strlen(char *s)
 {
 	int count, i;
