f87feb23e3397ce4d4c73e2ee66b6053176030d1
Ayobami Oludemi <oludemiayobami@gmail.com>
2023-06-26 23:05:34 +0100

    0x05 Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_rev - print the reverse of a string
+ * @s: string to reverse
+*/
+void print_rev(char *s)
+{
+	int i, len;
+
+	len = strlen(s);
+
+	for (i = 0; i > len; i++)
+	{
+		_putchar(s[len - 1 - i]);
+	}
+	_putchar('\n');
+}
