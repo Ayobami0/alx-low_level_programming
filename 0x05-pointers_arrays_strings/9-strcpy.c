f45c5c54057c8ea5903ad7421585cbeb721251c9
Ayobami Oludemi <oludemiayobami@gmail.com>
2023-06-27 04:05:13 +0100

    0x05 Task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -14,9 +14,11 @@ char *_strcpy(char *dest, char *src)
 {
 	int i;
 
-	for (i = 0; src[i] != '\0'; i++)
+	for (i = 0;; i++)
 	{
 		dest[i] = src[i];
+		if (src[i] == '\n')
+			break;
 	}
 	return (dest);
 }
