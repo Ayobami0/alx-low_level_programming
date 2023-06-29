5f9f63120f2186ca11d059f3f0b4e79f96d7561b
Ayobami Oludemi <oludemiayobami@gmail.com>
2023-06-29 20:26:35 +0100

    0x06 Task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncpy - copy string from @src to @dest
+ * and returns a pointer to @dest and terminates
+ * the result after @n of bytes
+ * @dest: destination string
+ * @src: string to copy
+ * @n: number of bytes to copy
+ *
+ * Return: pointer to @dest
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+
+	for (i = 0; src[i] != '\0' && i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
