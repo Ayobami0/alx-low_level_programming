d8d74d452025b96ed20b6ed632c38e9812c7383b
Ayobami Oludemi <oludemiayobami@gmail.com>
2023-06-29 22:28:43 +0100

    0x06 Task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcmp - compare string @s1 with @s2
+ * and returns a pointer to @dest and terminates
+ * @s1: string1
+ * @s2: string2
+ *
+ * Return: int
+*/
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		if (s1[i] < s2[i])
+			return (-1);
+		else if (s1[i] > s2[i])
+			return (1);
+	}
+	return (0);
+}
