e18d28eee6f61a4867958a759fb9b81c41f4ce0f
Ayobami Oludemi <oludemiayobami@gmail.com>
2023-06-29 22:06:45 +0100

    0x06 Task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <string.h>
 
 /**
  * _strncpy - copy string from @src to @dest
@@ -16,9 +15,14 @@ char *_strncpy(char *dest, char *src, int n)
 	int i;
 
 
-	for (i = 0; src[i] != '\0' && i < n; i++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
 	}
 
 	return (dest);
