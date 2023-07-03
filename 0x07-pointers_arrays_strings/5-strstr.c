dd52e7847a869ae5e672c261e5c339d844242db7
Ayobami Oludemi <oludemiayobami@gmail.com>
2023-07-04 00:37:09 +0100

    0x07 task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,1 +1,30 @@
-#include "5-strstr.h"
+#include "main.h"
+
+/**
+ * _strstr -  locates a substring
+ * @haystack: string to search in
+ * @needle: string to locate
+ *
+ * Return: pointer to substring
+*/
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j, k;
+	char *p;
+
+	p = 0;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (k = 0, j = i;;)
+		{
+			if (haystack[j++] != needle[k++])
+				break;
+			if (needle[k] == '\0')
+			{
+				p = haystack + i;
+			}
+		}
+	}
+	return (p);
+}
