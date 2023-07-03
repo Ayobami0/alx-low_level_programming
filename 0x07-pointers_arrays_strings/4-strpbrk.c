ee37c0ce65deaad5df373f8181e8021ffe8f02b7
Ayobami Oludemi <oludemiayobami@gmail.com>
2023-07-03 14:25:24 +0100

    0x07 task 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strpbrk - gets the length of a prefix substring
+ * @s: string
+ * @accept: character to search
+ *
+ * Return: pointer to first occurence of
+ * a char in accept or NULL if no match is found
+*/
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+	char *p;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (accept[i] == s[j])
+			{
+				p = s + i + 1;
+				return (p);
+			}
+		}
+	}
+	return (0);
+}
