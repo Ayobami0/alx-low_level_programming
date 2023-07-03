168a91c38f5ca801b17336d734c33d39cc57a387
Ayobami Oludemi <oludemiayobami@gmail.com>
2023-07-03 08:25:01 +0100

    0x07 task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strchr -  locates a character in a string
+ * @s: string
+ * @c: character to search
+ *
+ * Return: pointer to first occurence of
+ * @c or NULL if not found
+*/
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s ++[i] == c)
+			return (s);
+	}
+	return (NULL);
+}
