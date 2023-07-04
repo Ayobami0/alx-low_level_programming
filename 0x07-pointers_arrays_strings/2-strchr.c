d77a6e5ef0514c9907410de66019b55270b3112b
Ayobami Oludemi <oludemiayobami@gmail.com>
2023-07-04 19:47:11 +0100

    0x07 task 2 fix

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -12,11 +12,14 @@
 char *_strchr(char *s, char c)
 {
 	int i;
+	char *p;
+
+	p = s;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s ++[i] == c)
-			return (s);
+		if (*(++p) == c)
+			return (p);
 	}
 	return (NULL);
 }
