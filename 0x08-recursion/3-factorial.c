c4b69eb3829ec45f15e2c498d2d5d75ac135b6fb
Ayobami Oludemi <oludemiayobami@gmail.com>
2023-07-05 23:15:30 +0100

    0x08 task 3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * factorial - returns the factorial of a
+ * given number recursively
+ * @n: number
+ *
+ * Return: factorial value
+*/
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (1);
+	return (factorial(n - 1));
+}
+
+
