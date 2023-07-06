5b51a5f2bb190550370a8cb2c3fd15c089798cbf
Ayobami Oludemi <oludemiayobami@gmail.com>
2023-07-07 00:25:48 +0100

    0x08 task 5 & 6

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - gets the square root of a number
+ * recursively
+ * @n: number
+ *
+ * Return: square root
+*/
+int _sqrt_recursion(int n)
+{
+	return (1);
+}
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * is_prime_number - check if a number is prime
+ * recursively
+ * @n: number
+ *
+ * Return: 0 if not prime and
+ * 1 if prime
+*/
+int is_prime_number(int n)
+{
+	return (1);
+}
+
