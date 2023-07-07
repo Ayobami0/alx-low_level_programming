6236f4c6747636f1eeedff3bdf5845aa2c96e58e
Ayobami Oludemi <oludemiayobami@gmail.com>
2023-07-07 21:03:23 +0100

    0x0A task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include "stdio.h"
+
+/**
+ * main - entry point of this program
+ * prints the number of the arguments in a program
+ * followed by a newline
+ * @argc: number of argument
+ * @argv: the arguments
+ *
+ * Return: (0)
+*/
+int main(int argc, char **argv)
+{
+	(void) argv;
+
+	printf("%d\n", argc);
+	return (0);
+}
