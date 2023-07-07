39aa98c36f6550b8a92094a358ab0e2ae7790a90
Ayobami Oludemi <oludemiayobami@gmail.com>
2023-07-07 20:42:18 +0100

    0x0A task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of this program
+ * prints the name of the program followed by a
+ * newline
+ * @argc: number of argument
+ * @argv: the arguments
+ *
+ * Return: (0)
+*/
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
