edb9a65ebccb4187f0cfb46cc262ff6a87e2686e
Ayobami Oludemi <oludemiayobami@gmail.com>
2023-07-10 20:31:53 +0100

    0x0B task 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -28,7 +28,7 @@ int **alloc_grid(int width, int height)
 
 	for (i = 0; i < height; i++)
 	{
-		arr[i] = malloc(sizeof(int));
+		arr[i] = malloc(sizeof(int) * width);
 		if (arr[i] == NULL)
 			return (NULL);
 	}
