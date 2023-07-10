25c25d360252616ee98bb3d2048fe6be1a74d0a5
Ayobami Oludemi <oludemiayobami@gmail.com>
2023-07-11 00:08:02 +0100

    0x0B task 100 && 101

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * argstostr - concatenates all the arguments
+ * recived from main.
+ * @ac: argument count
+ * @av: argument values
+ *
+ * Return: pointer to the new string
+ * NULL if ac == 0 or av == NULL
+ * on error, return NULL
+*/
+char *argstostr(int ac, char **av)
+{
+	int i, j, k, total_len;
+	char *arr;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+		total_len += strlen(av[i]);
+
+	arr = malloc(sizeof(char) * total_len);
+
+	if (arr == NULL)
+		return (NULL);
+
+	k = 0;
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; j <= (int)strlen(av[i]); j++, k++)
+		{
+			if (j == (int)strlen(av[i]))
+				arr[k] = '\n';
+			else
+				arr[k] = av[i][j];
+		}
+	}
+	return (arr);
+}
---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,84 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int count_words(char *s);
+
+/**
+ * strtow - returns a pointer to an
+ * array of strings (words)
+ * @str: string to exract words from
+ *
+ * Return: pointer to an array of strings
+ * NULL if str == NULL or str == ""
+ * on error, return NULL
+*/
+char **strtow(char *str)
+{
+	int i, j, k, start_count, letter_count;
+	char **arr;
+
+	if (str[0] == 0 || str == NULL)
+		return (NULL);
+
+	start_count = letter_count = 0;
+
+	arr = malloc(
+		(sizeof(char *) * count_words(str)) + sizeof(char **));
+
+	if (arr == NULL)
+		return (NULL);
+
+	for (i = j = 0, start_count = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] != ' ')
+		{
+			letter_count++;
+			start_count = 1;
+		}
+		else if (str[i] == ' ' && start_count != 0)
+		{
+			/*printf("total letters in arr[%d]: %d\n", j, letter_count);*/
+			arr[j] = malloc(sizeof(char) * letter_count);
+			if (arr[j] == NULL)
+				return (NULL);
+			for (k = 0; k < letter_count; k++)
+			{
+				arr[j][k] = *(str + i - (letter_count - k));
+			}
+			j++;
+			start_count = 0;
+			letter_count = 0;
+		}
+	}
+	return (arr);
+}
+
+/**
+ * count_words - counts the number of words in a
+ * string.
+ * Words are seperated by space
+ * @s: string
+ *
+ * Return: number of words
+*/
+int count_words(char *s)
+{
+	int i, total_words, start_count;
+
+	total_words = start_count = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if ((s[i] != ' ') && (start_count == 0))
+		{
+			total_words++;
+			start_count = 1;
+		}
+		else if (s[i] == ' ')
+			start_count = 0;
+	}
+	/*printf("total words: %d\n", total_words);*/
+	return (total_words);
+}
