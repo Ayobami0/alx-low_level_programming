9742469e50064a20df52c8902ac7cea36297beb7
Ayobami Oludemi <oludemiayobami@gmail.com>
2023-07-13 18:53:48 +0100

    0x0C Task 3

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -17,7 +17,7 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 
-	arr = malloc(sizeof(int) * (max - min));
+	arr = malloc(sizeof(int) * (max - min) + sizeof(int));
 
 	if (arr == NULL)
 		return (NULL);
