8426fb94206b4ae0d808a994e067b857726d2c40
Ayobami Oludemi <oludemiayobami@gmail.com>
2023-07-14 16:44:03 +0100

    0x0C Task 101: Fix 1

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -5,32 +5,37 @@
 
 void print_str(char *str);
 char *rev_string(char *c);
-void mul(unsigned long n1, unsigned long n2);
-char *itoa(unsigned long n);
-int n_count(unsigned long n);
+void mul(char *n1, char *n2);
+int check_invalid_numbers(char *s);
+char *strip_zeros(char *s);
 
-int main(int argc, char *argv[]) {
-	unsigned long num1, num2;
-
+int main(int argc, char *argv[])
+{
 	if (argc != 3)
 	{
-		print_str("Failed");
+		print_str("Error");
 		exit(98);
 	}
-	if (atoi(argv[1]) == 0 || atoi(argv[2]) == 0)
+	if (check_invalid_numbers(argv[1]) != 0 ||
+		check_invalid_numbers(argv[2]) != 0)
 	{
-		print_str("Failed");
+		print_str("Error");
 		exit(98);
 	}
 
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[2]);
+	mul(argv[1], argv[2]);
 
-	mul(num1, num2);
-
-    return (0);
+	return (0);
 }
+int check_invalid_numbers(char *s)
+{
+	int i;
 
+	for (i = 0; s[i] != '\0'; i++)
+		if (!(s[i] >= 48 && s[i] <= 57))
+			return (1);
+	return (0);
+}
 
 void print_str(char *str)
 {
@@ -43,55 +48,68 @@ void print_str(char *str)
 	_putchar('\n');
 }
 
-void mul(unsigned long n1, unsigned long n2)
+void mul(char *n1, char *n2)
 {
-	unsigned long result;
-	char *str;
+	int *inter_str, carry, sum,
+		len_n1, len_n2, i, j;
+	char *result_str;
 
-	result = n1 * n2;
-	str = itoa(result);
-	print_str(str);
-	free(str);
-}
-char *itoa(unsigned long n)
-{
-	char *str;
-	int i;
 
-	str = malloc(n_count(n) + 1);
-	i = 0;
-	while (n != 0)
+	len_n1 = strlen(n1);
+	len_n2 = strlen(n2);
+
+	inter_str = malloc((len_n1 + len_n2) * sizeof(int));
+	result_str = malloc(len_n1 + len_n2);
+
+	n1 = rev_string(n1);
+	n2 = rev_string(n2);
+
+	for (i = 0; i < len_n1; i++)
+		for (j = 0; j < len_n2; j++)
+			inter_str[i + j] += (n1[i] - '0') * (n2[j] - '0');
+
+	for (i = carry = 0; i < len_n2 + len_n1; i++)
 	{
-		str[i] = '0' + (n % 10);
-		n /= 10;
-		i++;
+		sum = inter_str[i] + carry;
+		result_str[i] = (sum % 10) + '0';
+		carry = sum / 10;
 	}
-	str[i] = '\0';
-	str = rev_string(str);
-	return (str);
+	result_str[i] = '\0';
+
+	print_str(strip_zeros(rev_string(result_str)));
+	free(inter_str);
 }
+
 char *rev_string(char *c)
 {
-	char *str;
 	unsigned int i;
+	char zero = '0';
+	char temp;
 
-
-	str = malloc(strlen(c) + 1);
-
-	for (i = 0; i < strlen(c); i++)
-		str[i] = c[strlen(c) - i - 1];
-	str[i] = '\0';
-	return (str);
-}
-int n_count(unsigned long n)
-{
-	int count;
-
-	count = 0;
-	while (n != 0)
+	if (atoi(c) == 0)
 	{
-		count++;
-		n /= 10;
+		c = &zero;
+		return (c);
 	}
-	return (count);
+
+	for (i = 0; i < strlen(c) / 2; i++)
+	{
+		temp = c[i];
+		c[i] = c[strlen(c) - i - 1];
+		c[strlen(c) - i - 1] = temp;
+	}
+	return (c);
+}
+char *strip_zeros(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] != 48)
+			return (s);
+		s++;
+	}
+	return (s);
 }
