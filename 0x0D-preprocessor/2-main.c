b9b7b5bca6ff9059d0a3beac5fcfe77d2adb3641
Ayobami Oludemi <oludemiayobami@gmail.com>
2023-07-17 17:41:47 +0100

    0x0D Task 0 - 4

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+/**
+ * main - Main entry point of the program
+ * Description:
+ * Prints out the name of the file the program was compiled from
+ *
+ * Return: 0
+*/
+int main(void) {
+
+	printf("%s\n", __FILE__);
+    return (0);
+}
---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,6 @@
+#ifndef FUNCTION_LIKE_MACRO_H_
+#define FUNCTION_LIKE_MACRO_H_
+
+#define ABS(x) (if (x < 0) (x) * -1;x;)
+
+#endif /* FUNCTION_LIKE_MACRO_H_ */
