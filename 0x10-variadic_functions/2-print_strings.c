0de97271f6166dec531c827046ebb5e031a8718c
Ayobami Oludemi <oludemiayobami@gmail.com>
2023-07-20 22:19:45 +0100

    fix errors

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,22 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list v_args;
+	unsigned int i;
+
+
+	va_start(v_args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		if (va_arg(v_args, char *) == NULL)
+			printf("(nil)");
+		else
+			printf("%s", va_arg(v_args, char *));
+		if (separator != NULL && (i != n - 1))
+			printf("%s", separator);
+	}
+	printf("\n");
+}
--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,14 +1,18 @@
 #include "variadic_functions.h"
+#include <stdarg.h>
 #include <stdio.h>
+#include <string.h>
 
 void print_all(const char *const format, ...)
 {
 	va_list v_args;
 	unsigned int i;
+	char *str;
 
+	va_start(v_args, format);
 
 	i = 0;
-	while (format[i] != '\0')
+	while (i < strlen(format))
 	{
 		switch (format[i])
 		{
@@ -19,16 +23,26 @@ void print_all(const char *const format, ...)
 				printf("%i", va_arg(v_args, int));
 				break;
 			case 'f':
-				printf("%lf", va_arg(v_args, double));
+				printf("%f", va_arg(v_args, double));
 				break;
 			case 's':
-				if (va_arg(v_args, char *) == NULL)
-					printf("(nil)");
-				else
-					printf("%s", va_arg(v_args, char *));
+				str = va_arg(v_args, char *);
+				if (str != NULL)
+				{
+					printf("%s", str);
+					break;
+				}
+				printf("(nil)");
 				break;
 		}
+		if (i != strlen(format) - 1 && (format[i] == 'i'
+										|| format[i] == 'f'
+										|| format[i] == 's'
+										|| format[i] == 'c' ))
+			printf(", ");
 		i++;
 	}
+
 	printf("\n");
+	va_end(v_args);
 }
