2abed5df3a68d642132c9e7daa2bb9ec93a7c73c
Ayobami Oludemi <oludemiayobami@gmail.com>
2023-08-03 21:15:28 +0100

    Fix errors in task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,7 +1,11 @@
 #include "main.h"
 #include <math.h>
+#include <stdio.h>
 #include <string.h>
 
+
+
+unsigned int raise_pow(unsigned int num, int power);
 /**
  * binary_to_uint - convert a number in binary to unsigned int
  * @b: binary number has a pointer to an array of char
@@ -21,7 +25,15 @@ unsigned int binary_to_uint(const char *b)
 	for (i = 0; b[i] != '\0'; i++)
 	{
 		if (b[i] == '1')
-			result += pow(2, len - i - 1);
+		{
+			if ((len - i - 1) == 0)
+			{
+				result += 1;
+				break;
+			}
+			result += raise_pow(2, len - i - 1);
+
+		}
 		else if (b[i] == '0')
 			result += 0;
 		else
@@ -29,4 +41,30 @@ unsigned int binary_to_uint(const char *b)
 	}
 
 	return (result);
+}
+
+/**
+ * raise_pow - raise a number to a given power
+ * @num: number
+ * @power: power
+ *
+ * Return: the result of the calculation
+ */
+unsigned int raise_pow(unsigned int num, int power)
+{
+	/*Recursive solution*/
+	/* if (power == 0) */
+	/*return (num); */
+
+	/* Loop Solution */
+	/* num = (raise_pow(num, power--)); */
+	/* return (num); */
+	int i, pow;
+
+	pow = num;
+	for (i = 0; i < (power - 1); i++)
+	{
+		num *= pow;
+	}
+	return (num);
 }
