e23cb8d92ea544943aa4c69b715c30382b4f6521
Ayobami Oludemi <oludemiayobami@gmail.com>
2023-08-07 13:05:28 +0100

    First commit for 0x15-file_io

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <fcntl.h>
+#include <unistd.h>
+
+/**
+ * read_textfile - Read and write the contents of a
+ * text file to standard output.
+ *
+ * @filename: Pointer to the name of the text file to read.
+ * @letters: The maximum number of characters to read from the file.
+ *
+ * Return: The number of characters successfully
+ * read and written to the standard output,
+ * or 0 if an error occurs or if the file is empty.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+		ssize_t count;
+		int i, fd;
+		char buff;
+
+		if (filename == NULL)
+			return (0);
+
+		fd = open(filename, O_RDONLY);
+
+		while (read(fd, &buff, 1) > 0 && count != letters)
+		{
+			if (write(STDOUT_FILENO, &buff, 1) <= 0)
+			{
+				return (0);
+			}
+			count++;
+		}
+
+	return (count);
+}
