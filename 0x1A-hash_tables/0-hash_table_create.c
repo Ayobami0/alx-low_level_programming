99b187bb12e6612d6cfa0809a9873c911423f4d8
Ayobami Oludemi <oludemiayobami@gmail.com>
2023-10-05 22:12:51 +0100

    Add 3-hash-table-set

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,6 +10,7 @@
 hash_table_t *hash_table_create(unsigned long int size) {
   hash_table_t *hash_table;
   hash_node_t **array;
+  unsigned int i;
 
   hash_table = malloc(sizeof(hash_table_t));
   if (hash_table == NULL)
@@ -17,6 +18,9 @@ hash_table_t *hash_table_create(unsigned long int size) {
   array = malloc(sizeof(hash_node_t *) * size);
   if (array == NULL)
     return (NULL);
+
+  for (i = 0; i < size; i++)
+    array[i] = NULL;
 
   hash_table->array = array;
   hash_table->size = size;
