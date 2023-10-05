76567195acb030885ec8340c301bf3a365c38d2c
Ayobami Oludemi <oludemiayobami@gmail.com>
2023-10-05 20:03:42 +0100

    fixes

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -4,7 +4,6 @@
 hash_table_t *hash_table_create(unsigned long int size) {
   hash_table_t *hash_table;
   hash_node_t **array;
-  unsigned long int i;
 
   hash_table = malloc(sizeof(hash_table_t));
   array = malloc(sizeof(hash_node_t *) * size);
