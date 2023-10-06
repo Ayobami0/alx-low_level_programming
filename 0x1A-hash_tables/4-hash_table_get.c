22799bfff8001b67f220737829f355e749dc38f0
Ayobami Oludemi <oludemiayobami@gmail.com>
2023-10-07 00:06:14 +0100

    Task 4 fix

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -16,6 +16,9 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int idx;
 	hash_node_t *selected_node;
 
+	if (ht == NULL || key == NULL)
+		return (NULL);
+
 	idx = key_index((unsigned char *)key, ht->size);
 
 	if (idx >= ht->size)
